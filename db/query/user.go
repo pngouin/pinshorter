0bd0ef60aa770bdae6600a9d65c5a99286692a7a
Pingouin <me@pingouin.io>
2020-02-03 17:38:28 +0100

    Change all user queries to make username in lowercase.

--- db/query/user.go
+++ db/query/user.go
@@ -1,11 +1,11 @@
 package query
 
 const UserCreate = `
-INSERT INTO users (user_id, name, password, created_at) VALUES ($1, $2, $3, now())
+INSERT INTO users (user_id, name, password, created_at) VALUES ($1, lower($2), $3, now())
 `
 
 const UserExist = `
-SELECT count(*) FROM users WHERE user_id=$1 and name=$2 AND deleted_at is null
+SELECT count(*) FROM users WHERE user_id=$1 and name=lower($2) AND deleted_at is null
 `
 
 const UserById = `
@@ -13,5 +13,5 @@ SELECT user_id, name FROM user WHERE user_id=$1 AND deleted_at IS NULL
 `
 
 const UserConnection = `
-SELECT user_id, count(*) from users where name=$1 and password=$2 and deleted_at is null group by user_id
+SELECT user_id, count(*) from users where name=lower($1) and password=$2 and deleted_at is null group by user_id
 `
