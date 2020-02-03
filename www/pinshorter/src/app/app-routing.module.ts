f37eb336dcd06107ab2ed38e59c78947a2ddc720
Pingouin <me@pingouin.io>
2020-02-03 20:35:12 +0100

    Style improvement.

--- www/pinshorter/src/app/app-routing.module.ts
+++ www/pinshorter/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
-import { LoginComponent } from './auth/login/login.component';
 import { LinksComponent } from './pages/links/links.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
--- www/pinshorter/src/app/pages/pages.module.ts
+++ www/pinshorter/src/app/pages/pages.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LinksComponent } from './links/links.component';
 import { LinkModule } from '../link/link.module';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { LoginComponent } from './login/login.component';
+import { AuthModule } from '../auth/auth.module';
 
 
 
 @NgModule({
-  declarations: [LinksComponent],
+  declarations: [LinksComponent, LoginComponent],
   imports: [
     CommonModule,
-    LinkModule
+    LinkModule,
+    NgZorroAntdModule,
+    AuthModule
   ],
   exports: [
     LinksComponent
